8a985f7f85c41ce9c04a16310fc8d57da169e953
icalvin102 <calvin@icalvin.de>
2020-09-03 22:24:48 +0200

    added nf2json.sh and description

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # nerdfonts2jsonsvg
 Convert nerd-font-icons to SVG / JSON-SVG / CSV
+
+The script will download `Symbols-2048-em Nerd Font Complete.ttf` from the
+[nerd-fonts Project](https://github.com/ryanoasis/nerd-fonts) converts it to
+SVG, JSON and CSV. 
+
+The exported JSON and CSV files respectivly contain all the data needed 
+to dynamically create `<svg>`-Tags with JavaScript.
+
+**Structure of `.json` file**
+
+``` json
+[
+    // ...
+    {
+      "viewbox": "-10 0 1300 2048",
+      "d": "M1280 827l-1152 1152l384 -896h-512l1152 -1152l-384 896h512z",
+      "glyph": "⚡",
+      "name": "oct-zap",
+      "codepoint": "26a1"
+    },
+    // ...
+]
+```
+
+## Usage
+
+If all the dependencies are installed you can simply run:
+
+`bash <(curl -s 'https://raw.githubusercontent.com/icalvin102/nerdfonts2jsonsvg/master/nf2json.sh')`
+
+
+Alternativly you can download the
+[nf2json.sh](https://raw.githubusercontent.com/icalvin102/nerdfonts2jsonsvg/master/nf2json.sh)
+script and run it locally:
+
+`bash nf2json.sh`
+
+
+This will create `nerdfonts_svg.json`, `nerdfonts_svg.csv` and a `svg/` directory with all
+the icons seperated into their own files. The downloaded files from
+[nerd-fonts Project](https://github.com/ryanoasis/nerd-fonts) will be in `nerdfons/`.
+
+
+### Dependencies
+
+To run `nf2json.sh` the packages `curl`, `awk`, `jq` and  `fontforge` need to be installed first.
+
+If not already installed the packages should be availible in all the major packetmanagers.
+
+
+**Install dependencies on Arch**
+
+Run: `sudo pacman -S curl gawk jq fontforge`
+
+**Install dependencies on Debian/Ubuntu**
+
+Run: `sudo apt-get install curl gawk jq fontforge`
+
+
+
+
+
---
+++ nf2json.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+
+mkdir nerdfonts
+
+curl 'https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/src/glyphs/Symbols-2048-em%20Nerd%20Font%20Complete.ttf' \
+    -o nerdfonts/Symbols-2048-em_Nerd_Font_Complete.ttf
+
+curl 'https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/css/nerd-fonts-generated.css' \
+    -o nerdfonts/nerd-fonts-generated.css
+
+
+mkdir svg
+
+fontforge -quiet -lang=ff -c 'Open($1); SelectWorthOutputting(); foreach Export("svg/%u.svg"); endloop;' nerdfonts/Symbols-2048-em_Nerd_Font_Complete.ttf 
+
+echo 'created svg/*.svg'
+
+tmp_glyphs="$(mktemp)"
+
+awk '
+    BEGIN {
+        OFS = "; ";
+        print "codepoint", "glyph", "name";
+    }
+
+    /^.nf-/ {
+        match($0, /^.nf-(.*):/, n);
+    }
+
+    /\scontent:/ {
+        match($0, /\\(.*)"/, c);
+        print c[1], "\\u"c[1], n[1];
+    }
+' nerdfonts/nerd-fonts-generated.css > "$tmp_glyphs"
+
+echo -e "$(<"$tmp_glyphs")" > "$tmp_glyphs"
+
+tmp_paths="$(mktemp)"
+
+awk '
+    BEGIN {
+        OFS = "; "
+        print "codepoint", "viewbox", "d";
+    }
+
+    /viewBox/ {
+        match($0, /viewBox="(.*)"/, vb)
+    }
+
+    /d=/,/\/>/ {
+        out = out" "$0;
+    }
+
+    /\/>/ {
+        if(match(out, /d="(.*)"/, d)){
+            match(FILENAME, /svg\/(.*).svg/, fn);
+            print fn[1], vb[1], d[1];
+        }
+        out = ""
+    }
+' svg/*.svg > "$tmp_paths"
+
+
+tmp_glyphs_json="$(mktemp)"
+tmp_paths_json="$(mktemp)"
+
+jq --raw-input --slurp '[split("\n") | .[1:-1][] | split("; ") | {(.[0]): {glyph: .[1], name: .[2]} }] | add' "$tmp_glyphs" > "$tmp_glyphs_json"
+
+jq --raw-input --slurp '[split("\n") | .[1:-1][] | split("; ") | {(.[0]): {viewbox: .[1], d: .[2]} }] | add' "$tmp_paths" > "$tmp_paths_json"
+
+jq --slurp '[ .[0] * .[1] | to_entries | .[] | select(.value.name != null) | .value.codepoint = .key | .value]' "$tmp_paths_json" "$tmp_glyphs_json" > nerdfonts_svg.json
+echo "created nerdfonts_svg.json"
+
+jq -r '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv' nerdfonts_svg.json > nerdfonts_svg.csv
+echo "created nerdfonts_svg.csv"
+
+
+
