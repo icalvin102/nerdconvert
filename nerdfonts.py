86b058598b8a5f425e4142aadb12231187271545
icalvin102 <calvin@icalvin.de>
2020-09-08 02:32:24 +0200

    added field renaming/modifiers and filter

--- nerdfonts.py
+++ nerdfonts.py
@@ -23,6 +23,12 @@ def save_json(filepath, data):
     with open(filepath, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
+def camel(match):
+    return match.group(1) + match.group(2).upper()
+
+
+def to_camel_case(string):
+    return re.sub(r'(.*?)[-_](\w)', camel, string)
 
 def combine_dict(a, b):
     return {**a, **b}
@@ -75,7 +81,7 @@ def extract_from_glyph(glyph):
     return {
         'code': get_code(glyph),
         'glyphname': glyph.glyphname,
-        'boundingbox': glyph.boundingBox()
+      #  'boundingbox': glyph.boundingBox()
     }
 
 
@@ -111,9 +117,43 @@ def remove_unnamed(data):
     return {k:v for (k, v) in data.items() if v.get('name')}
 
 
-def filter_fields(data, fields):
-    return [{k:v for (k, v) in record.items() if k in fields} for record in data ]
+def modify_fields(data, fields):
+    modifiers = {
+        'camelcase': to_camel_case,
+        'upper': lambda x: x.upper(),
+        'lower': lambda x: x.lower(),
+    }
 
+    split_fields = [f.split(':') for f in fields]
+    modified_fields = [f for f in split_fields if len(f) > 1]
+
+    result = []
+    for record in data:
+        new_record = dict(record)
+        for f in modified_fields:
+            current = new_record[f[0]]
+            for m in f[2:]:
+                current = modifiers.get(m, lambda x: x)(current)
+            new_record[f[1]] = current
+        result.append(new_record)
+
+    return result
+
+
+def filter_fields(data, fields):
+    field_names = [f.split(':') for f in fields]
+    field_names = [f[0] if len(f) < 2 else f[1] for f in field_names]
+    print(field_names)
+    return [{f:record[f] for f in field_names if f in record} for record in data ]
+
+def match_filters(record, filters):
+    for f in filters:
+        if not re.match(f[1], record[f[0]]):
+            return False
+    return True
+
+def filter_records(data, filters):
+    return [record for record in data if match_filters(record, filters)]
 
 def create_raw_data(resources, force_download=False, svgdir='svg'):
     download_resources(resources, force_download)
@@ -142,36 +182,56 @@ def create_raw_data(resources, force_download=False, svgdir='svg'):
     return list(data.values())
 
 
-def to_csv(data):
-    pass
+def export_csv(filepath, data, fieldnames):
+    import csv
+
+    with open(filepath, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerows(data)
 
 
 def to_json(data):
     pass
 
 
-def to_es(data):
+def export_es(data):
     pass 
 
 
 def parse_args():
 
-    fields = ['code', 'name', 'glyphname', 'glyph', 'svgfile', 'viewbox', 'boundingbox', 'paths']
+    fields = ['code', 'name', 'glyphname', 'glyph', 'svgfile', 'viewbox', 'paths']
 
     parser = argparse.ArgumentParser(
-            description='Convert nerd-font-icons to SVG / JSON / CSV / ESModule')
+            description='Convert nerd-font-icons to SVG / JSON / CSV / ESModule',
+            formatter_class=argparse.RawTextHelpFormatter)
 
-    parser.add_argument('--download-dir', default='nerdfonts_dl/', type=str,
-                        help='Download Directory for nerd-fonts resources')
+    parser.add_argument('--download', default='/tmp/nerdfonts_dl/', type=str,
+                        help='Download Directory for nerd-fonts resources (ttf/css)')
 
-    parser.add_argument('--svg-dir', default='svg/', type=str,
+    parser.add_argument('--svg', default='', type=str, metavar='SVG_FILE',
                         help='Export Directory for the *.svg files')
 
-    parser.add_argument('--output-json', default='nerd-fonts.json', type=str,
+    parser.add_argument('--json', default='nerd-fonts.json', type=str, metavar='JSON_FILE',
                         help='Filename of the *.json output')
 
-    parser.add_argument('--fields', default=fields, type=str,
-                        help='One or more fields that will be included in the output file.\nPossible values '+', '.join(fields))
+    parser.add_argument('--csv', default='', type=str, metavar='CSV_FILE',
+                        help='Filename of the *.csv output')
+
+    parser.add_argument('--es', default='', type=str, metavar='ESMODULE_FILE',
+                        help='Filename of the *.js output')
+
+    parser.add_argument('--fields', default=fields, type=str, nargs='*', metavar='FIELDNAME[:REPLACEMENT[:MODIFIER]]',
+                        help='One or more fields that will be included in the output file.\n'
+                            'A field can be specified in the form of FIELDNAME[:REPLACEMENT[:MODIFIER]]\n'
+                            'e.g. name:iconname:camelcase will include the "name"-field,\n'
+                            'rename it to "iconname" and convert the fieldvalues to camelcase.\n'
+                            'Possible FIELDNAMEs are '+', '.join(fields))
+
+    parser.add_argument('--filter', nargs=2, type=str, metavar=('FIELD', 'REGEX'), action='append',
+                        help='Filter FIELD by REGEX (can be used multiple times)')
 
     return parser.parse_args()
 
@@ -183,20 +243,26 @@ def main():
     resources = {
         'fontfile': {
             'url': 'https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/src/glyphs/Symbols-2048-em%20Nerd%20Font%20Complete.ttf',
-            'filepath': os.path.join(args.download_dir, 'Symbols-2048-em_Nerd_Font_Complete.ttf')
+            'filepath': os.path.join(args.download, 'Symbols-2048-em_Nerd_Font_Complete.ttf')
             },
         'cssfile': {
             'url': 'https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/css/nerd-fonts-generated.css',
-            'filepath': os.path.join(args.download_dir, 'nerd-fonts-generated.css')
+            'filepath': os.path.join(args.download, 'nerd-fonts-generated.css')
             }
         }
 
 
-    raw_data = create_raw_data(resources, False, args.svg_dir)
+    raw_data = create_raw_data(resources, False, '/tmp/nerdfonts_svg/')
 
-    data = filter_fields(raw_data, fields)
+    data = modify_fields(raw_data, args.fields)
+    data = filter_records(data, args.filter)
+    data = filter_fields(data, args.fields)
+    #data = raw_data
+    
+    if args.csv:
+        export_csv(args.csv, data, args.fields)
 
-    save_json(args.output_json, data)
+    save_json(args.json, data)
 
     print('done!')
 
