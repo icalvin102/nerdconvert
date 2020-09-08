cd5cd9e90439154eb480f9aaa22e30dc8f88051d
icalvin102 <calvin@icalvin.de>
2020-09-09 01:11:30 +0200

    added formatters

--- nerdfonts.py
+++ nerdfonts.py
@@ -8,10 +8,11 @@ import xml.dom.minidom
 from functools import reduce
 
 
+
 def download_resources(resources, force=False):
     for (name, resource) in resources.items():
         if os.path.isfile(resource['filepath']) and not force:
-            print('File exists. Skip download', name, ':', resource['filepath'])
+            print('Skip download', name, ':', resource['filepath'])
         else:
             os.makedirs(os.path.dirname(resource['filepath']), exist_ok=True)
             print('Downloading', name, ':', resource['url'])
@@ -19,9 +20,18 @@ def download_resources(resources, force=False):
             print('Saved', name, ':', resource['filepath'])
 
 
+def save_file(filepath, content):
+    dirname = os.path.dirname(filepath)
+    if dirname:
+        os.makedirs(dirname, exist_ok=True)
+    with open(filepath, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+
 def save_json(filepath, data):
     with open(filepath, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+
 
 def camel(match):
     return match.group(1) + match.group(2).upper()
@@ -30,8 +40,18 @@ def camel(match):
 def to_camel_case(string):
     return re.sub(r'(.*?)[-_](\w)', camel, string)
 
+
 def combine_dict(a, b):
     return {**a, **b}
+
+
+def dict_to_js(data):
+    json_data = json.dumps(data, ensure_ascii=False)
+    return re.sub(r'"([a-zA-Z_]*)":', r'\1:', json_data)
+
+
+def dict_to_json(data):
+    return json.dumps(data, ensure_ascii=False, indent=4)
 
 
 def combine_tables(*tables):
@@ -116,35 +136,54 @@ def extract_from_svgs(svgfiles):
 def remove_unnamed(data):
     return {k:v for (k, v) in data.items() if v.get('name')}
 
-
-def modify_fields(data, fields):
-    modifiers = {
-        'camelcase': to_camel_case,
-        'upper': lambda x: x.upper(),
-        'lower': lambda x: x.lower(),
-    }
-
-    split_fields = [f.split(':') for f in fields]
-    modified_fields = [f for f in split_fields if len(f) > 1]
-
-    result = []
-    for record in data:
-        new_record = dict(record)
-        for f in modified_fields:
-            current = new_record[f[0]]
-            for m in f[2:]:
-                current = modifiers.get(m, lambda x: x)(current)
-            new_record[f[1]] = current
-        result.append(new_record)
-
-    return result
+modifiers = {
+    'camelcase': to_camel_case,
+    'upper': lambda x: x.upper(),
+    'lower': lambda x: x.lower(),
+}
 
 
-def filter_fields(data, fields):
-    field_names = [f.split(':') for f in fields]
-    field_names = [f[0] if len(f) < 2 else f[1] for f in field_names]
-    print(field_names)
-    return [{f:record[f] for f in field_names if f in record} for record in data ]
+class FieldFormatter:
+    def __init__(self, field_description, rename=True):
+        field = field_description.split(':')
+        self.name = field[0]
+        if rename:
+            self.new_name = field[1] if len(field) > 1 else field[0]
+            self.modifiers = [modifiers[m] for m in field[2:] if m in modifiers]
+        else:
+            self.new_name = field[0]
+            self.modifiers = [modifiers[m] for m in field[1:] if m in modifiers]
+    
+    def apply_modifiers(self, value):
+        for m in self.modifiers:
+            value = m(value)
+        return value
+    
+    def format(self, record):
+        value = record.get(self.name, None)
+        return (self.new_name, self.apply_modifiers(value)) if value else None
+    
+
+class FilenameFormatter:
+    def __init__(self, filename):
+        self.format_string = re.sub(r'\{(\w+)[a-zA-Z:]*\}', r'{\1}', filename)
+        field_descriptions = re.findall(r'\{([a-zA-Z:]+)\}', filename)
+        self.field_formatters = [FieldFormatter(fd, False) for fd in field_descriptions]
+
+    def format(self, record):
+        formatted_fields = [f.format(record) for f in self.field_formatters] 
+        replacements = {f[0]:f[1] for f in formatted_fields if f}
+        return self.format_string.format(**replacements)
+
+
+class RecordFormatter:
+    def __init__(self, field_descriptions):
+        self.field_formatters = [FieldFormatter(fd, True) for fd in field_descriptions] 
+
+    def format(self, record):
+        formatted_fields = [f.format(record) for f in self.field_formatters] 
+        return {f[0]:f[1] for f in formatted_fields if f}
+
 
 def match_filters(record, filters):
     for f in filters:
@@ -161,15 +200,18 @@ def create_raw_data(resources, force_download=False, svgdir='svg'):
     font = fontforge.open(resources['fontfile']['filepath'])
 
     table = extract_from_css(resources['cssfile']['filepath'])
-    print('Extracted iconinfo from cssfile:', resources['cssfile']['filepath'])
+    print('Extracted iconinfo from cssfile:',
+            resources['cssfile']['filepath'])
 
     glyph_data = extract_from_glyphs(get_glyphs(font))
-    print('Extracted iconinfo from fontfile:', resources['fontfile']['filepath'])
+    print('Extracted iconinfo from fontfile:',
+            resources['fontfile']['filepath'])
     
     svgdir = os.path.join(svgdir, '')
     os.makedirs(svgdir, exist_ok=True)
     svg_files = generate_svgs(get_glyphs(font), svgdir)
-    print('Generated svgicons from fontfile:', resources['fontfile']['filepath'], '=>', svgdir+'*.svg')
+    print('Generated svgicons from fontfile:',
+            resources['fontfile']['filepath'], '=>', svgdir+'*.svg')
 
     svg_file_data = extract_from_svgs(svg_files)
     print('Extracted inconpaths from svgfiles:', svgdir+'*.svg')
@@ -182,56 +224,107 @@ def create_raw_data(resources, force_download=False, svgdir='svg'):
     return list(data.values())
 
 
-def export_csv(filepath, data, fieldnames):
-    import csv
+def split_path(path, extension=None, default_filename=None):
+    if extension and default_filename and not path.endswith(extension):
+        path = os.path.join(path, default_filename+extension)
 
-    with open(filepath, 'w', newline='') as csvfile:
+    dirname, filename = os.path.split(path)
+    while r'{' in dirname:
+        dirname, fn = os.path.split(dirname)
+        filename = os.path.join(fn, filename)
+    return (dirname, filename)
+        
+
+def export_csv(filepath, data, record_formatter):
+    import csv
+    base_dir, file_name = split_path(filepath, '.csv', 'nerdfonts')
+    fieldnames = [ff.new_name for ff in record_formatter.field_formatters]
+    export_data = [record_formatter(record) for record in data]
+
+    with open(os.path.join(base_dir, file_name), 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
-        writer.writerows(data)
+        writer.writerows(export_data)
+
+    return data
 
 
-def to_json(data):
-    pass
+def export_json(filepath, data, record_formatter):
+    base_dir, file_name = split_path(filepath, '.json', 'nerdfonts')
+    export_data = [record_formatter.format(record) for record in data]
+    print(os.path.join(base_dir, file_name))
+    save_file(os.path.join(base_dir, file_name), dict_to_json(export_data))
+
+    return data
 
 
-def export_es(data):
-    pass 
+def export_svg(filepath, data, record_formatter):
+    import shutil
+    base_dir, file_name = split_path(filepath, '.svg', '{code}_{name}')
+    
+    filename_formatter = FilenameFormatter(os.path.join(base_dir, file_name))
+
+    for record in data:
+        filename = filename_formatter.format(record)
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        shutil.copy(record['svgfile'], filename)
+        record['svgfile'] = filename
+
+    return data
 
 
+def export_es(filepath, data, record_formatter):
+    base_dir, file_name = split_path(filepath, '.js', '{name}')
+
+    filename_formatter = FilenameFormatter(file_name)
+    index_js = ''
+    
+    name_formatter = FieldFormatter('name:camelcase', False)
+
+    for record in data:
+        filtered_record = record_formatter.format(record)
+        filename = filename_formatter.format(record)
+        content = 'export default ' + dict_to_js(filtered_record) 
+        module_name = name_formatter.format(record)[1]
+        save_file(os.path.join(base_dir, filename), content)
+        index_js += 'export { default as '+ module_name +' }'
+        index_js += 'from \''+ os.path.join('.', filename) +'\';\n'
+
+    save_file(os.path.join(base_dir, 'index.js'), index_js)
+
+    return data
+        
 def parse_args():
 
-    fields = ['code', 'name', 'glyphname', 'glyph', 'svgfile', 'viewbox', 'paths']
+    fields = ['code', 'name', 'glyphname',
+                'glyph', 'svgfile', 'viewbox', 'paths']
 
     parser = argparse.ArgumentParser(
-            description='Convert nerd-font-icons to SVG / JSON / CSV / ESModule',
-            formatter_class=argparse.RawTextHelpFormatter)
+        description='Convert nerd-font-icons to SVG / JSON / CSV / ESModule',
+        formatter_class=argparse.RawTextHelpFormatter)
 
     parser.add_argument('--download', default='/tmp/nerdfonts_dl/', type=str,
-                        help='Download Directory for nerd-fonts resources (ttf/css)')
+        help='Download Directory for nerd-fonts resources (ttf/css)')
 
-    parser.add_argument('--svg', default='', type=str, metavar='SVG_FILE',
-                        help='Export Directory for the *.svg files')
+    
+    parser.add_argument('--fields', default=fields, type=str, nargs='*',
+        metavar='FIELDNAME[:REPLACEMENT[:MODIFIER]]',
+        help='One or more fields that will be included in the '
+        'output file.\n A field can be specified in the form of'
+        'FIELDNAME[:REPLACEMENT[:MODIFIER]]\n' 
+        'e.g. name:iconname:camelcase will include the "name"-field,\n'
+        'rename it to "iconname" and convert the fieldvalues to camelcase.\n'
+        'Possible FIELDNAMEs are '+', '.join(fields))
 
-    parser.add_argument('--json', default='nerd-fonts.json', type=str, metavar='JSON_FILE',
-                        help='Filename of the *.json output')
+    parser.add_argument('--filter', nargs=2, type=str,
+        metavar=('FIELD', 'REGEX'), action='append',
+        help='Filter FIELD by REGEX (can be used multiple times)')
 
-    parser.add_argument('--csv', default='', type=str, metavar='CSV_FILE',
-                        help='Filename of the *.csv output')
-
-    parser.add_argument('--es', default='', type=str, metavar='ESMODULE_FILE',
-                        help='Filename of the *.js output')
-
-    parser.add_argument('--fields', default=fields, type=str, nargs='*', metavar='FIELDNAME[:REPLACEMENT[:MODIFIER]]',
-                        help='One or more fields that will be included in the output file.\n'
-                            'A field can be specified in the form of FIELDNAME[:REPLACEMENT[:MODIFIER]]\n'
-                            'e.g. name:iconname:camelcase will include the "name"-field,\n'
-                            'rename it to "iconname" and convert the fieldvalues to camelcase.\n'
-                            'Possible FIELDNAMEs are '+', '.join(fields))
-
-    parser.add_argument('--filter', nargs=2, type=str, metavar=('FIELD', 'REGEX'), action='append',
-                        help='Filter FIELD by REGEX (can be used multiple times)')
+    
+    parser.add_argument('-o', '--output', default=[['json', 'nf__.json']],
+        type=str, nargs='+', action='append',
+        metavar=('FORMAT', 'FILEPATH'), help='Output')
 
     return parser.parse_args()
 
@@ -240,13 +333,16 @@ def main():
 
     args = parse_args()
 
+    base_url = 'https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master'
+
     resources = {
         'fontfile': {
-            'url': 'https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/src/glyphs/Symbols-2048-em%20Nerd%20Font%20Complete.ttf',
-            'filepath': os.path.join(args.download, 'Symbols-2048-em_Nerd_Font_Complete.ttf')
+            'url': base_url+'/src/glyphs/Symbols-2048-em%20Nerd%20Font%20Complete.ttf',
+            'filepath': os.path.join(args.download,
+                'Symbols-2048-em_Nerd_Font_Complete.ttf')
             },
         'cssfile': {
-            'url': 'https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/css/nerd-fonts-generated.css',
+            'url': base_url+'/css/nerd-fonts-generated.css',
             'filepath': os.path.join(args.download, 'nerd-fonts-generated.css')
             }
         }
@@ -254,15 +350,27 @@ def main():
 
     raw_data = create_raw_data(resources, False, '/tmp/nerdfonts_svg/')
 
-    data = modify_fields(raw_data, args.fields)
-    data = filter_records(data, args.filter)
-    data = filter_fields(data, args.fields)
-    #data = raw_data
+    data = filter_records(raw_data, args.filter) if args.filter else raw_data
     
-    if args.csv:
-        export_csv(args.csv, data, args.fields)
+    record_formatter = RecordFormatter(args.fields)
+    
+    exporters = {
+            'json': export_json,
+            'csv': export_csv,
+            'svg': export_svg,
+            'es': export_es
+            }
 
-    save_json(args.json, data)
+    for output in args.output:
+        exporter = exporters.get(output[0], None)
+        if exporter:
+            print('running exporter', output[0])
+            exporter(output[1], data, record_formatter)
+        else:
+            print(f'exporter for format"{output[0]}"does not exist')
+
+    
+    #save_json(args.json, data)
 
     print('done!')
 
